2d4f8311cbc1eb3135a9348baa7a27a004493b91
kenny0619 <kehindeibukun0619@gmail.com>
2020-08-16 00:21:57 +0100

    set up the schema for the users

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-  extends: 'airbnb-base',
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+    'linebreak-style': 'off',
+  },
 };
---
+++ server/models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: 'user',
+    enum: ['admin', 'rider', 'user', 'partner'],
+  },
+  accessToken: {
+    type: String,
+  },
+});
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
