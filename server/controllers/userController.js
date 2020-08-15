bcca1767bfb3d2d618f7999ed2f85e0b5a27cfbb
kenny0619 <kehindeibukun0619@gmail.com>
2020-08-16 00:59:14 +0100

    created user login logic

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -27,3 +27,23 @@ exports.signup = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return next(new Error('Email does not exist'));
+    const validPassword = await validatePassword(password, user.password);
+    if (!validPassword) return next(new Error('Password is not correct'));
+    const accessToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '1d',
+    });
+    await User.findByIdAndUpdate(user._id, { accessToken });
+    res.status(200).json({
+      data: { email: user.email, role: user.role },
+      accessToken,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
